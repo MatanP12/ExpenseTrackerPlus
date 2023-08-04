b5eeca0480ccd1364bf729f36d76416d3bf98a3d
Matan Peretz <matanperetz@outlook.com>
2023-08-04 21:02:44 +0300

    Adding Server Calls
    - Installing JSON-Server for mocking a server.
    - Installing Axios to make HTTP requests.
    - Creating Custom Hooks to server calls of GET, POST, DELETE, PUT.
    - Modifing some pages accordinglly.

---
+++ src/utilities/ServerCalls.js
@@ -0,0 +1,70 @@
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:3004',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+function useGetData(url, onSuccessResponse = (data) => console.log(data), onErrorResponse = (error) => console.log(error)) {
+    const [isLoading, setIsLoading] = useState(false);
+    useEffect(() => {
+        async function fetchData() {
+            setIsLoading(true);
+            try {
+                const response = await axiosInstance.get(url);
+                onSuccessResponse(response.data);
+            } catch (err) {
+                onErrorResponse(err)
+            }
+            finally {
+                setIsLoading(false);
+            }
+        }
+        fetchData();
+    }, []);
+    return isLoading;
+}
+
+function usePostData(url, onSuccessResponse = (data) => console.log(data), onErrorResponse = (error) => console.log(error)) {
+
+    const handlePostData = useCallback(async (expense) => {
+        try {
+            const response = await axiosInstance.post(url, expense);
+            onSuccessResponse(response.data);
+        } catch (err) {
+            onErrorResponse(err);
+        }
+    }, [])
+    return handlePostData;
+}
+
+function useDeleteData(url, onSuccessResponse = (data) => console.log(data), onErrorResponse = (error) => console.log(error)) {
+    const handleDeleteData = useCallback(async (data) => {
+        try {
+            const response = await axiosInstance.delete(url + "/" + data.id);
+            onSuccessResponse(response.data);
+
+        } catch (err) {
+            onErrorResponse(err);
+        }
+    }, [url]);
+    return handleDeleteData;
+}
+
+function usePutData(url, onSuccessResponse = (data) => console.log(data), onErrorResponse = (error) => console.log(error)) {
+    const handlePutData = useCallback(async (data) => {
+        try {
+            const response = await axiosInstance.put(url + "/" + data.id);
+            onSuccessResponse(response.data);
+        } catch (err) {
+            onErrorResponse(err);
+        }
+    }, [url]);
+    return handlePutData;
+}
+
+export { useGetData, usePostData, useDeleteData, usePutData }
